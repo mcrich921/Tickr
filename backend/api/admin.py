e2c1c0df939a3f58d131f075437e7e0db5132b96
mcrich921 <mcrich921@gmail.com>
2025-02-23 11:59:52 -0500

    updated admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Stock, Transaction, UserProfile
 
-# Register your models here.
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('ticker', 'name', 'price')  # Shows these fields in the list view
+    search_fields = ('ticker', 'name')  # Allows searching by ticker or name
+    ordering = ('ticker',)  # Default ordering by ticker
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'stock', 'transaction_type', 'quantity', 'price', 'timestamp')
+    list_filter = ('transaction_type','stock',)  # Add a filter for transaction type (BUY/SELL)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'balance')  # Display user and balance in the list view
+    search_fields = ('user__username',)  # Allow searching by username
+    ordering = ('user',)  # Default ordering by username
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+admin.site.register(Stock, StockAdmin)
+admin.site.register(Transaction, TransactionAdmin)
