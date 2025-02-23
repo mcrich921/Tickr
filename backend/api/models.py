9473886bd5aac8655973cae6cffb7553cdc8ba15
mcrich921 <mcrich921@gmail.com>
2025-02-23 16:43:33 -0500

    added comment

--- backend/api/models.py
+++ backend/api/models.py
@@ -37,6 +37,7 @@ class Transaction(models.Model):
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+# Create a UserProfile when a new User is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
