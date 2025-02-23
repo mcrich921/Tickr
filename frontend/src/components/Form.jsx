9d10d3728cf2439c8bb9f6b0ce7433ce499fc321
mcrich921 <mcrich921@gmail.com>
2025-02-23 13:51:49 -0500

    added errors for forms

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -7,12 +7,20 @@ import "../styles/Form.css";
 function Form({ route, method }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null); // State for error messages
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     setLoading(true);
+    setError(null);
     e.preventDefault();
+
+    if (!username || !password) {
+      setError("Please enter a username and password.");
+      setLoading(false);
+      return;
+    }
 
     try {
       const res = await api.post(route, { username, password });
@@ -24,6 +32,7 @@ function Form({ route, method }) {
         navigate("/login");
       }
     } catch (error) {
+      setError(error);
       console.log(error);
     } finally {
       setLoading(false);
@@ -33,6 +42,8 @@ function Form({ route, method }) {
   return (
     <form onSubmit={handleSubmit} className="form-container">
       <h1>{method === "login" ? "Login" : "Register"}</h1>
+      {/* Error message */}
+      {error && <p className="error-message">{error}</p>}
       <input
         className="form-input"
         type="text"
