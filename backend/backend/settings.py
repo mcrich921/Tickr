17bae9172368e052c68ed1e7f37c91e0aa26cb31
mcrich921 <mcrich921@gmail.com>
2025-03-04 20:27:07 -0500

    postgresql connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -99,9 +100,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mock_trading',      
+        'USER': 'trading_user',          
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),               
+        'HOST': 'localhost',          
+        'PORT': '5432',         # Default PostgreSQL port   
     }
 }
 
